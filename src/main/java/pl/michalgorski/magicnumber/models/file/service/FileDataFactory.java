0a037ca2bc67592ad62f8b2a98af2fa38df33c8d
MichalG11 <grsk11@wp.pl>
2020-02-08 01:27:44 +0100

    work with FileDataFactory

--- src/main/java/pl/michalgorski/magicnumber/models/file/service/FileDataFactory.java
+++ src/main/java/pl/michalgorski/magicnumber/models/file/service/FileDataFactory.java
@@ -1,19 +1,26 @@
 package pl.michalgorski.magicnumber.models.file.service;
 
 import pl.michalgorski.magicnumber.models.file.data.FileData;
+import pl.michalgorski.magicnumber.models.file.data.FilePatternModel;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class FileDataFactory {
 
     private ByteReadService byteReadService;
+    private FileExtensionService fileExtensionService;
+    private FilePatternModel filePatternModel;
+    private final Map<String, int[]> mapWithFilePattern;
 
     public FileDataFactory(){
         byteReadService = new ByteReadService();
+        fileExtensionService = new FileExtensionService();
+        filePatternModel = new FilePatternModel();
+        mapWithFilePattern = filePatternModel.getMapWithFilePattern();
     }
+
     public List<FileData> createFileDataList(List<File> list) throws IOException {
 
         List<FileData> listOfFileData = new ArrayList<>();
@@ -29,9 +36,57 @@ public class FileDataFactory {
     private FileData createFileData(File file) throws IOException {
 
         String fileName = file.getName();
-
+        String fileExtension = fileExtensionService.getExtensionOfFile(file);
+        boolean isFileExtensionSupported = isFileExtensionSupported(fileExtension);
         byte[] realByteArray = byteReadService.getBytesOfFile(file);
+
+        String realExtension = "";
 
         return null;
     }
+
+    private boolean isFileExtensionSupported(String fileExtension) {
+
+        return mapWithFilePattern.containsKey(fileExtension);
+    }
+
+    private String getRealExtension(byte[] byteArray) {
+
+        String realExtension = "";
+
+        if (byteArray.length > 0){
+            int lengthOfPatternArray = 3;
+            int[] realIntArray = new int[lengthOfPatternArray];
+
+            for (int i = 0; i < lengthOfPatternArray; i++) {
+                realIntArray[i] = Byte.toUnsignedInt(byteArray[i]);
+            }
+            for (Map.Entry<String, int[]> entry : mapWithFilePattern.entrySet()) {
+
+                if (Arrays.equals(realIntArray, entry.getValue())) {
+                    realExtension = entry.getKey();
+                    break;
+                }
+            }
+        }
+        return realExtension;
+    }
+
+    private String getRealExtensionForTxtFile(byte[] byteArray){
+
+        String realExtension = "TXT";
+        int currentIntValue;
+        if (byteArray.length > 0){
+            for (byte currentByteValue : byteArray) {
+                currentIntValue = Byte.toUnsignedInt(currentByteValue);
+
+                if (currentIntValue < 32 || currentIntValue > 127) {
+                    realExtension = "";
+                    break;
+                }
+            }
+        }
+        return realExtension;
+    }
+
 }
